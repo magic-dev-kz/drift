16e9292dd5d2fbc53a5b027c6b3688c54cb31428
AImagic <ai_magic@MacBook-Air-Ila.local>
2026-03-29 15:02:24 +0700

    Drift v2.1: ARIA tabs, welcome dialog, focus-visible, delete UX

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'drift-v2';
+const CACHE = 'drift-v2.1';
 const ASSETS = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
